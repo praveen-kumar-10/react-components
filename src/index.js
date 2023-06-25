362440ae858395e5b8cd820df63479030c933400
N Praveen Kumar <praveen@Ns-MacBook-Air.local>
2023-06-25 12:22:40 +0530

    Added Docs for Button

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
+import App from "./AppD";
 // import App from "./AppTesting";
-import App from "./App";
+// import App from "./App";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles/main.scss";
